42612f0782d342ff9c593058207e26f0f22ef59b
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2020-11-14 02:36:20 +0100

    Add files via upload

--- ajoute.js
+++ ajoute.js
@@ -1,7 +1,14 @@
+const { DiscordAPIError, MessageCollector } = require("discord.js");
+
 module.exports = {
     name : 'ajoute',
     description : 'ajoute le lien yt a la playlist yt',
     execute(message) {
-        message.channel.send("La musique a été ajoutée a la playlist youtube de campus en musique !"); 
+        if(channel.name === `partage_de_sons`)
+            events = message.collect();
+            MessageCollector.handleCollect(events);
+
+            if(events === message.end())
+                message.channel.send("La musique a été ajoutée a la playlist youtube de campus en musique !"); 
     }
 }
