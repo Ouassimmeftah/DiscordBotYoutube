1ed851c3fa82e5d2ed2d71614f73d1a6d145f7ba
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2020-11-14 02:50:31 +0100

    Add files via upload

--- ajoute.js
+++ ajoute.js
@@ -5,10 +5,11 @@ module.exports = {
     description : 'ajoute le lien yt a la playlist yt',
     execute(message) {
         if(channel.name === `partage_de_sons`)
+          if( message.content.startswith(prefixeLienYoutube1) || message.content.startswith(prefixeLienYoutube2))
             events = message.collect();
             MessageCollector.handleCollect(events);
 
-            if(events === message.end())
+            if(events === message.end() )
                 message.channel.send("La musique a été ajoutée a la playlist youtube de campus en musique !"); 
     }
 }
